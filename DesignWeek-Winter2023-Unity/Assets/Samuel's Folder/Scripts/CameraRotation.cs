f79e3ce21dfc2d784ed213d8070d1ae766baf73b
samuelw0624 <samuelw0624@hotmail.com>
2023-03-07 16:56:24 -0500

    Complete camera rotation
    added camera rotation functionalities

--- DesignWeek-Winter2023-Unity/Assets/Samuel's Folder/Scripts/CameraRotation.cs
+++ DesignWeek-Winter2023-Unity/Assets/Samuel's Folder/Scripts/CameraRotation.cs
@@ -1,18 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraRotation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject mCamera;
+    public GameObject leftButton;
+    public GameObject rightButton;
+    [SerializeField] float rotationSpeed;
+    Vector3 targetEulerAngles;
+    Vector3 currentEulerAngles;
+    [SerializeField] float y;
+    //float yAngleModulus;
+    bool turnLeft;
+    bool turnRight;
 
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log(currentEulerAngles);
+        //Debug.Log(targetEulerAngles);
+        //Debug.Log(y);
+        currentEulerAngles += new Vector3(0, y, 0) * Time.deltaTime * rotationSpeed;
+        mCamera.transform.eulerAngles = currentEulerAngles;
+
+        if(turnLeft)
+        {
+            if (currentEulerAngles.y >= targetEulerAngles.y)
+            {
+                y = -1;
+                leftButton.GetComponent<Button>().interactable = false;
+            }
+            else if (currentEulerAngles.y <= targetEulerAngles.y)
+            {
+                y = 0;
+                turnLeft = false;
+                leftButton.GetComponent<Button>().interactable = true;
+            }
+        }
+        if(turnRight)
+        {
+            if (currentEulerAngles.y <= targetEulerAngles.y)
+            {
+                y = 1;
+                rightButton.GetComponent<Button>().interactable = false;
+            }
+            else if (currentEulerAngles.y >= targetEulerAngles.y)
+            {
+                y = 0;
+                turnRight = false;
+                rightButton.GetComponent<Button>().interactable = true;
+            }
+        }
         
+        
+    }
+
+    public void RotateCameraLeft()
+    {
+        turnLeft = true;
+        currentEulerAngles = mCamera.transform.eulerAngles;
+        targetEulerAngles = currentEulerAngles + new Vector3(0, -90, 0);
+    }
+
+    public void RotateCamerRight()
+    {
+        turnRight = true;
+        currentEulerAngles = mCamera.transform.eulerAngles;
+        targetEulerAngles = currentEulerAngles + new Vector3(0, 90, 0);
+    }
+
+    IEnumerator RotateLeft()
+    {
+        yield return null;
     }
 }

db4e682f8b6b81bf55740ef0504802453c807b35
samuelw0624 <samuelw0624@hotmail.com>
2023-03-08 11:38:53 -0500

    Setup title screen and scene transition
    added placeholder title screen and added scene transition functionalities

--- DesignWeek-Winter2023-Unity/Assets/Samuel's Folder/Scripts/SceneTransitionManager.cs
+++ DesignWeek-Winter2023-Unity/Assets/Samuel's Folder/Scripts/SceneTransitionManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Door : MonoBehaviour
+public class SceneTransitionManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -19,6 +19,15 @@ public class Door : MonoBehaviour
 
     private void OnMouseDown()
     {
+        ToGallery();
+    }
+    
+    public void ToGallery()
+    {
         SceneManager.LoadScene("Title Screen");
+    }
+    public void ToStudio()
+    {
+        SceneManager.LoadScene("Samuel's Test Scene");
     }
 }

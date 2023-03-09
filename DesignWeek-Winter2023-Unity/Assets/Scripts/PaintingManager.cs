8a562da296550826c99db472dc381bb054e090c6
Samuel Wang <samuelw0624@hotmail.com>
2023-03-09 15:45:13 -0500

    Add painting SO class

--- DesignWeek-Winter2023-Unity/Assets/Scripts/PaintingManager.cs
+++ DesignWeek-Winter2023-Unity/Assets/Scripts/PaintingManager.cs
@@ -6,16 +6,29 @@ public class PaintingManager : MonoBehaviour
 {
     public GameObject PaintingFrame;
     [SerializeField] List<Material> paintingMats;
+    List<Material> usedMats;
+    Material currentMat;
+    bool movedPainting;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        currentMat = PaintingFrame.GetComponent<MeshRenderer>().material;
+    }
     void Start()
     {
-        PaintingFrame.GetComponent<MeshRenderer>().material = PaintingRandomizer();
+        currentMat = PaintingRandomizer();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(paintingMats.Count == 0)
+        {
+            foreach (Material usedMat in usedMats)
+            {
+                paintingMats.Add(usedMat);
+            }
+        }
     }
     Material PaintingRandomizer()
     {
@@ -26,6 +39,15 @@ public class PaintingManager : MonoBehaviour
     public void NextPainting()
     {
         //save current painting
-        PaintingFrame.GetComponent<MeshRenderer>().material = PaintingRandomizer();
+        if(movedPainting)
+        {
+            currentMat = PaintingRandomizer();
+        }
+    }
+    void MovePainting()
+    {
+        usedMats.Add(currentMat);
+        paintingMats.Remove(currentMat);
+        movedPainting = true;
     }
 }

39c5caf1cd97a77b5fb6c1edf775d1acae34d9e9
Samuel Wang <samuelw0624@hotmail.com>
2023-03-09 16:07:18 -0500

    Change paint frame to quad

--- DesignWeek-Winter2023-Unity/Assets/Scripts/PaintingManager.cs
+++ DesignWeek-Winter2023-Unity/Assets/Scripts/PaintingManager.cs
@@ -6,29 +6,19 @@ public class PaintingManager : MonoBehaviour
 {
     public GameObject PaintingFrame;
     [SerializeField] List<Material> paintingMats;
-    List<Material> usedMats;
+    [SerializeField] List<Material> usedMats;
     Material currentMat;
     bool movedPainting;
     // Start is called before the first frame update
-    private void Awake()
-    {
-        currentMat = PaintingFrame.GetComponent<MeshRenderer>().material;
-    }
     void Start()
     {
-        currentMat = PaintingRandomizer();
+        PaintingFrame.GetComponent<MeshRenderer>().material = PaintingRandomizer();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(paintingMats.Count == 0)
-        {
-            foreach (Material usedMat in usedMats)
-            {
-                paintingMats.Add(usedMat);
-            }
-        }
+
     }
     Material PaintingRandomizer()
     {
@@ -38,16 +28,12 @@ public class PaintingManager : MonoBehaviour
     }
     public void NextPainting()
     {
-        //save current painting
-        if(movedPainting)
-        {
-            currentMat = PaintingRandomizer();
-        }
+        PaintingFrame.GetComponent<MeshRenderer>().material = PaintingRandomizer();
     }
-    void MovePainting()
-    {
-        usedMats.Add(currentMat);
-        paintingMats.Remove(currentMat);
-        movedPainting = true;
-    }
+    //void MovePainting()
+    //{
+    //    usedMats.Add(PaintingFrame.GetComponent<MeshRenderer>().material);
+    //    paintingMats.Remove(PaintingFrame.GetComponent<MeshRenderer>().material);
+    //    movedPainting = true;
+    //}
 }

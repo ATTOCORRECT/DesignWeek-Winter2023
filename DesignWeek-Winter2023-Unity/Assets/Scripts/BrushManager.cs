3cc1c0f90b7c7e36028422502b9dc7118297dd4b
Auto <97545662+ATTOCORRECT@users.noreply.github.com>
2023-03-09 14:56:52 -0500

    Updated the brush manager with new settings

--- DesignWeek-Winter2023-Unity/Assets/Scripts/BrushManager.cs
+++ DesignWeek-Winter2023-Unity/Assets/Scripts/BrushManager.cs
@@ -16,27 +16,27 @@ public class BrushManager : MonoBehaviour
         switch (brushIndex) 
         {
             case 0: // cat          texture to use | type (1-stroke, 2-stamp) | degrees default rotation | 0 - 1 rotation randomization | pixel position variation x & y | pixel size | pixel size variation +/- value    
-                PaintingCanvas.GetComponent<PaintingCanvasManager>().setBrushPattern(brushTextures[0], 1, 0, 0.1f, new Vector2(0, 0), 50, 10);
+                PaintingCanvas.GetComponent<PaintingCanvasManager>().setBrushPattern(brushTextures[0], 2, 0, 0.5f, new Vector2(0, 0), 100, 20);
                 break;
 
             case 1: // computer mouse
-                PaintingCanvas.GetComponent<PaintingCanvasManager>().setBrushPattern(brushTextures[1], 1, 0, 0.1f, new Vector2(0, 0), 50, 10);
+                PaintingCanvas.GetComponent<PaintingCanvasManager>().setBrushPattern(brushTextures[1], 2, 0, 0.1f, new Vector2(0, 0), 80, 0);
                 break;
 
             case 2: // scrub daddy
-                PaintingCanvas.GetComponent<PaintingCanvasManager>().setBrushPattern(brushTextures[2], 1, 0, 0.1f, new Vector2(0, 0), 50, 10);
+                PaintingCanvas.GetComponent<PaintingCanvasManager>().setBrushPattern(brushTextures[2], 2, 0, 0.1f, new Vector2(0, 0), 90, 10);
                 break;
 
             case 3: // banana
-                PaintingCanvas.GetComponent<PaintingCanvasManager>().setBrushPattern(brushTextures[3], 1, 0, 0.1f, new Vector2(0, 0), 50, 10);
+                PaintingCanvas.GetComponent<PaintingCanvasManager>().setBrushPattern(brushTextures[3], 2, 0, 0.3f, new Vector2(0, 0), 100, 15);
                 break;
 
             case 4: // boxing glove
-                PaintingCanvas.GetComponent<PaintingCanvasManager>().setBrushPattern(brushTextures[4], 1, 0, 0.1f, new Vector2(0, 0), 50, 10);
+                PaintingCanvas.GetComponent<PaintingCanvasManager>().setBrushPattern(brushTextures[4], 2, 0, 0.1f, new Vector2(0, 0), 300, 30);
                 break;
 
             case 5: // boot
-                PaintingCanvas.GetComponent<PaintingCanvasManager>().setBrushPattern(brushTextures[5], 1, 0, 0.1f, new Vector2(0, 0), 50, 10);
+                PaintingCanvas.GetComponent<PaintingCanvasManager>().setBrushPattern(brushTextures[5], 2, 0, 0.1f, new Vector2(0, 0), 200, 10);
                 break;
 
             case 6: // eraser
@@ -44,7 +44,7 @@ public class BrushManager : MonoBehaviour
                 break;
 
             case 7: // bread
-                PaintingCanvas.GetComponent<PaintingCanvasManager>().setBrushPattern(brushTextures[7], 1, 0, 0.1f, new Vector2(0, 0), 50, 10);
+                PaintingCanvas.GetComponent<PaintingCanvasManager>().setBrushPattern(brushTextures[7], 2, 0, 0.1f, new Vector2(0, 0), 50, 10);
                 break;
 
             case 8: // grass
@@ -56,11 +56,11 @@ public class BrushManager : MonoBehaviour
                 break;
 
             case 10: // rose
-                PaintingCanvas.GetComponent<PaintingCanvasManager>().setBrushPattern(brushTextures[10], 1, 0, 0.1f, new Vector2(0, 0), 50, 10);
+                PaintingCanvas.GetComponent<PaintingCanvasManager>().setBrushPattern(brushTextures[10], 2, 0, 0.1f, new Vector2(0, 0), 50, 10);
                 break;
 
             case 11: // spatula
-                PaintingCanvas.GetComponent<PaintingCanvasManager>().setBrushPattern(brushTextures[11], 1, 0, 0.1f, new Vector2(0, 0), 50, 10);
+                PaintingCanvas.GetComponent<PaintingCanvasManager>().setBrushPattern(brushTextures[11], 2, 0, 0.1f, new Vector2(0, 0), 50, 10);
                 break;
 
             case 12: // sponge
